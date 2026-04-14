f5d2145a550d1327fb3a24799ca98a68a66b45f3
KKodiac <35219323+KKodiac@users.noreply.github.com>
2026-04-14 15:57:29 +0900

    Add five-nine formula v0.1.0

---
+++ Formula/five-nine.rb
@@ -0,0 +1,21 @@
+class FiveNine < Formula
+  desc "Claude service status TUI monitor"
+  homepage "https://github.com/KKodiac/five-nine"
+  version "0.1.0"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/KKodiac/five-nine/releases/download/v#{version}/five-nine-aarch64-apple-darwin"
+    sha256 "e2cb7b834de195445a3a3dea7e3dc786a9839f91c20c2024dcd44d008909d049"
+  else
+    url "https://github.com/KKodiac/five-nine/releases/download/v#{version}/five-nine-x86_64-apple-darwin"
+    sha256 "4e5de16d78b72e521d38c7c9ce347e2e589514ea5137bd896ddeffb1c803206a"
+  end
+
+  def install
+    bin.install "five-nine"
+  end
+
+  test do
+    assert_predicate bin/"five-nine", :exist?
+  end
+end

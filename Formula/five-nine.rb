3fff90a3f1cdd4b7caf6264cb873a7c67d63ef1b
KKodiac <35219323+KKodiac@users.noreply.github.com>
2026-04-14 17:30:21 +0900

    five-nine 0.2.3

--- Formula/five-nine.rb
+++ Formula/five-nine.rb
@@ -1,14 +1,14 @@
 class FiveNine < Formula
   desc "Claude service status TUI monitor"
   homepage "https://github.com/KKodiac/five-nine"
-  version "0.2.2"
+  version "0.2.3"
 
   if Hardware::CPU.arm?
     url "https://github.com/KKodiac/five-nine/releases/download/v#{version}/five-nine-aarch64-apple-darwin"
-    sha256 "5d62a417f1649cd94d09dfff76c475fd82c6259d5b54a7eb3ab7cabf8581c334"
+    sha256 "322587fd9e051009a16756db528a00fbf5ba8eb3437dc841fe208e60722179f1"
   else
     url "https://github.com/KKodiac/five-nine/releases/download/v#{version}/five-nine-x86_64-apple-darwin"
-    sha256 "d2bed5221c69905c687f7509807b1b52d95237c22194649279fd7ad5cbcd54a6"
+    sha256 "a202027bdd1de1fdee81089b611686682af7b4a7c46632e6f6886f70006086a6"
   end
 
   def install
